48b2c63c1bf20e38703d33ae71aa993577fbb17f
Win32x <win32.kido@gmail.com>
2018-01-12 12:27:12 +0300

    принятый файл от Андрея

---
+++ live.py
@@ -0,0 +1,44 @@
+import requests
+from pprint import pprint as p
+
+# https://yobit.net/api/3/depth/esp_eth
+
+a = lambda x: sum(float(i[1]) for i in requests.get('https://api.livecoin.net/exchange/order_book?currencyPair={}'.format(x)).json()['asks'])
+x = 'BIO/USD'
+orders = requests.get('https://api.livecoin.net/exchange/order_book?currencyPair={}'.format(x)).json()
+p(x)
+print(orders['asks'][0][0])
+print(orders['bids'][0][0])
+
+prices = {}
+for c in ['ETH', 'BTC']:
+    x = '{}/USD'.format(c)
+    orders = requests.get('https://api.livecoin.net/exchange/order_book?currencyPair={}'.format(x)).json()
+    # print(x)
+    prices[c + 'a'] = float(orders['asks'][0][0])
+    prices[c + 'b'] = float(orders['bids'][0][0])
+    # print(orders['asks'][0][0])
+    # print(orders['bids'][0][0])
+
+
+    x = 'BIO/{}'.format(c)
+    orders = requests.get('https://api.livecoin.net/exchange/order_book?currencyPair={}'.format(x)).json()
+    print(x)
+    print(round(float(orders['asks'][0][0]) * prices[c + 'a'], 5), orders['asks'][0][1])
+    print(round(float(orders['bids'][0][0]) * prices[c + 'b'], 5))
+
+for c in ['RUR']:
+    x = 'USD/{}'.format(c)
+    orders = requests.get('https://api.livecoin.net/exchange/order_book?currencyPair={}'.format(x)).json()
+    # print(x)
+    prices[c + 'a'] = float(orders['asks'][0][0])
+    prices[c + 'b'] = float(orders['bids'][0][0])
+    # print(orders['asks'][0][0])
+    # print(orders['bids'][0][0])
+
+
+    x = 'BIO/{}'.format(c)
+    orders = requests.get('https://api.livecoin.net/exchange/order_book?currencyPair={}'.format(x)).json()
+    print(x)
+    print(round(float(orders['asks'][0][0]) / prices[c + 'a'], 5), orders['asks'][0][1])
+    print(round(float(orders['bids'][0][0]) / prices[c + 'b'], 5))
