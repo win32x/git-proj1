e3c60d93776890b2301bb40c052ee995c32a8272
Win32x <win32.kido@gmail.com>
2018-01-12 12:52:35 +0300

    добавил ввод крипты но пока ошибка при отсутсвующей паре

--- live.py
+++ live.py
@@ -4,7 +4,8 @@ from pprint import pprint as p
 # https://yobit.net/api/3/depth/esp_eth
 
 a = lambda x: sum(float(i[1]) for i in requests.get('https://api.livecoin.net/exchange/order_book?currencyPair={}'.format(x)).json()['asks'])
-x = 'BIO/USD'
+y = (input('введите необходимую крипту\n')).upper()
+x = y+'/USD'
 orders = requests.get('https://api.livecoin.net/exchange/order_book?currencyPair={}'.format(x)).json()
 p(x)
 print(orders['asks'][0][0])
@@ -21,7 +22,7 @@ for c in ['ETH', 'BTC']:
     # print(orders['bids'][0][0])
 
 
-    x = 'BIO/{}'.format(c)
+    x = y+'/{}'.format(c)
     orders = requests.get('https://api.livecoin.net/exchange/order_book?currencyPair={}'.format(x)).json()
     print(x)
     print(round(float(orders['asks'][0][0]) * prices[c + 'a'], 5), orders['asks'][0][1])
@@ -37,7 +38,7 @@ for c in ['RUR']:
     # print(orders['bids'][0][0])
 
 
-    x = 'BIO/{}'.format(c)
+    x = y+'/{}'.format(c)
     orders = requests.get('https://api.livecoin.net/exchange/order_book?currencyPair={}'.format(x)).json()
     print(x)
     print(round(float(orders['asks'][0][0]) / prices[c + 'a'], 5), orders['asks'][0][1])
